50ad9b56f69f02ac9e9dd0da52ca7dd07a4f8ed8
unsmartly <111931454+unsmartly@users.noreply.github.com>
2022-10-03 11:47:55 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(400,400);
+s1=new Student("John",12,6)
+s2=new Student("Kat",6,7)
+}
+
+function draw() 
+{
+  background(30);
+  s1.display()
+  s2.display()
+}
